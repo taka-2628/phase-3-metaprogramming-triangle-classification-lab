1872e65426bc88922f5e63a9fa03a3e50c0620e8
Takashi <th2826@columbia.edu>
2022-03-19 22:20:30 -0400

    commit tree b9e098892a8ba38cf3846ee46198dc6ed3cdc764

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,65 @@
 class Triangle
-  # write code here
+  attr_reader :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    validate_triangle
+    if one == two && two == three
+      :equilateral
+    elsif one == two || two == three || one == three
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    if one * two * three <= 0
+      raise TriangleError
+    elsif (one + two <= three) || (two + three <= one) || (one + three <= two)
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+# class Triangle
+#   attr_reader :one, :two, :three
+
+#   def initialize(one, two, three)
+#     @one = one
+#     @two = two
+#     @three = three
+#   end
+
+#   def kind
+#     validate_triangle
+#     if one == two && two == three
+#       :equilateral
+#     elsif one == two || two == three || one == three
+#       :isosceles
+#     else
+#       :scalene
+#     end
+#   end
+
+#   def validate_triangle
+#     real_triangle = [(one + two > three), (one + three > two), (two + three > one)]
+
+#     [one, two, three].each do |side|
+#       real_triangle << false if side <= 0 
+#     end
+
+#     raise TriangleError if real_triangle.include?(false)
+#   end
+
+#   class TriangleError < StandardError
+#   end
+# end
